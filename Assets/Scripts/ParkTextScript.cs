6c9e1840dec9c5f13361198fd735008096a554cb
Muhammed Fasil K <fasilkalloth@gmail.com>
2023-10-05 18:42:12 +0530

    Fixed Bugs

--- Assets/Scripts/ParkTextScript.cs
+++ Assets/Scripts/ParkTextScript.cs
@@ -1,13 +1,19 @@
+
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ParkTextScript : MonoBehaviour
 {
-
-    private void Start()
+    private TextMeshProUGUI parkText;
+    private IEnumerator Start()
     {
         GameManager.Instance.parkText = this;
+        parkText = GetComponent<TextMeshProUGUI>();
+
+        yield return new WaitForSeconds(6);
+
+        parkText.text = "Press 'P' to Park";
         gameObject.SetActive(false);
 
     }
